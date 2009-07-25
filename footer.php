50e11717b734e9a9313e54a1f00452e2ef616f49
Florian Fiegel <florianfiegel@Br4inXt3nsi0n.(none)>
2009-07-25 03:10:26 +0200

    Cleaned Footer

--- footer.php
+++ footer.php
@@ -1,20 +1,9 @@
         <div id="footer">
-            <p>Proudly powered by <a href="http://habariproject.org/" title="Habari">Habari</a> | Theme by <a href="http://florian-fiegel.net">Florian Fiegel</a> | Logo by <a href="http://markusmeier.de">Markus Meier</a></p>
-            <p><a href="http://www.blogoscoop.net/blog/<?php echo $site['bs']; ?>"><img src="http://stats.blogoscoop.net/<?php echo $site['bs']; ?>/12.gif"
-          alt="blogoscoop"></a></p>
+            <p>Proudly powered by <a href="http://habariproject.org/" title="Habari">Habari</a> | Theme <a href="http://openthe.me" title="Get OpenTheme">OpenTheme</a> by <a href="http://florian-fiegel.net">Florian Fiegel</a>
             <?php $theme->footer(); ?>
         </div>
         <?php if ( !$loggedin ) : ?>
-        <script type="text/javascript">
-            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-        </script>
-        <script type="text/javascript">
-            try {
-                var pageTracker = _gat._getTracker("UA-9336357-<?php echo $site['ga']; ?>");
-                pageTracker._trackPageview();
-            } catch(err) {}
-        </script>
+<!--Place Analytics- / Tracker-Scripts here -- Tracking if not logged in! -->
         <?php endif; ?>
     </body>
 </html>
