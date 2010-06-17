971b522a9fcd26c25641ef424c692c6d1be575ae
Florian Fiegel <mail@ffiegel.net>
2010-06-18 00:42:42 +0200

    Update for Header Modification

--- footer.php
+++ footer.php
@@ -3,7 +3,15 @@
             <?php $theme->footer(); ?>
         </div>
         <?php if ( !$loggedin ) : ?>
-<!--Place Analytics- / Tracker-Scripts here -- Tracking if not logged in! -->
+			<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+			</script>
+			<script type="text/javascript">
+			try {
+			var pageTracker = _gat._getTracker("UA-6113559-1");
+			pageTracker._trackPageview();
+			} catch(err) {}</script>
         <?php endif; ?>
     </body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,15 @@
     <div id="sidebar">
     <!-- Call your plugins theme functions here-->
         <?php $theme->area('sidebar'); ?>
+		<?php $theme->twitter(); ?>
         <?php $theme->show_recentcomments(); ?>
+		<div class="block">
+            <h2>Get Habari!</h2>
+            <p><a href="http://dist.habariproject.org/habari-0.6.2.zip">Download der aktuellen stabilen Version von Habari</a><br />Weitere Informationen zu Habari gibt es natürlich <a href="http://www.habarimag.de/habari">hier</a> oder direkt beim <a href="http://habariproject.org">Habari-Projekt</a></p>
+        </div>
+		<div class="block">
+            <h2>Vernetzung</h2>
+            <p><a href="http://www.bloggeramt.de" title="Blogverzeichnis - BloggerAmt.de"><img src="http://www.bloggeramt.de/images/blog_button12.gif" alt="Bloggeramt.de" /></a> <a href="http://www.blogoscoop.net/blog/3704"><img src="http://stats.blogoscoop.net/3704/8.gif" alt="blogoscoop"/></a> <a href="http://blogpingr.de" title="BlogPingR.de: Blog Ping-Dienst, Blogmonitor"><img src="http://blogpingr.de/track/blogtrack/buttons/4/" alt="BlogPingR.de - Blog Ping-Dienst, Blogmonitor" /></a> <a href="http://www.twingly.com/blogs/profile?url=www.habarimag.de" title="Twingly BlogRank"><img src="http://www.twingly.com/badge/blogrank?url=www.habarimag.de" alt="Twingly BlogRank" /></a> 
+			<a href="http://blogalm.de/" title="Blogverzeichnis"><img src="http://blogalm.de/images/blogalm_button_rund.gif" border="0" alt="Blogverzeichnis" /></a> <a href="http://www.frischgebloggt.de/"><img src="http://www.frischgebloggt.de/button.png" alt="frisch gebloggt" /></a><script src="http://widgets.technorati.com/t.js" type="text/javascript"></script><a href="http://technorati.com/blogs/www.habarimag.de?sub=tr_authority_t_ns" class="tr_authority_t_js" style="color:#4261DF">View blog authority</a> <a href="http://www.seitwert.de/?url=www.habarimag.de" title="Seitwert von www.habarimag.de"><img src="http://img.seitwert.de/5f/e6/5fe63a3a875d976b883a14aebae813c3_green.png" alt="Seitwert" /></a></p>
+        </div>
     </div>
