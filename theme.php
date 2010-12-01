39ed5dfddf536f259a5779c64a944b9bd29c9654
Konzertheld <konzertheld@konzertheld.de>
2010-12-01 22:37:22 +0100

    Fixed a bug in OpenTheme where autop was not applied to excerpts.

--- theme.php
+++ theme.php
@@ -8,6 +8,7 @@ class opentheme extends Theme
 	{
 		// Apply Format::autop() to post content...
 		Format::apply( 'autop', 'post_content_out' );
+		Format::apply( 'autop', 'post_content_excerpt' );
 		// Apply Format::autop() to comment content...
 		Format::apply( 'autop', 'comment_content_out' );
 		// Truncate content excerpt at "more" …
