7b1f990d05e4743836abe99b58d454ca6e4dfdcb
Florian Fiegel <mail@fiegel.io>
2010-12-06 14:08:13 +0100

    Updated Info in Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <div id="footer">
-            <p>Proudly powered by <a href="http://habariproject.org/" title="Habari">Habari</a> | Theme <a href="http://openthe.me" title="Get OpenTheme">OpenTheme</a> by <a href="http://florian-fiegel.net">Florian Fiegel</a>
+            <p>Proudly powered by <a href="http://habariproject.org/" title="Habari">Habari</a> | Theme <a href="https://github.com/HabariMag/OpenTheme" title="Get OpenTheme">OpenTheme</a> by <a href="http://habarimag.de">HabariMag</a>
             <?php $theme->footer(); ?>
         </div>
     </body>
