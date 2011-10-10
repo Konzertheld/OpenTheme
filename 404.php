bc04d90b8d584532bd9b99ae0f1ceaf75a028577
Florian Fiegel <mail@fiegel.io>
2011-10-10 14:07:09 +0200

    fixed layout of 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
-<?php include 'header.php'; ?>
-
-	<div id="content">
-	<h2 class="center"><?php _e( "Whoops! 404" ); ?></h2>
-	   <p class="center"><?php _e( "The page you were trying to access is not really there. Please try again." ); ?><p>
-						
-    </div>
-<?php include'sidebar.php' ?>
-			
-<?php include 'footer.php'; ?>
+		<?php include 'header.php'; ?>
+		<div id="page">
+    		<div id="main">
+        		<div class="post">
+            		<h2><?php _e( "Whoops! 404" ); ?></h2>
+            		<p><?php _e( "The page you were trying to access is not really there. Please try again." ); ?></p>
+        		</div>
+    		</div>
+    		<?php include 'sidebar.php' ?>
+		</div>
+		<?php include 'footer.php'; ?>
