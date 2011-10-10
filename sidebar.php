c88623f83ab1fbaa0828fd8ee39b3e73425db609
Florian Fiegel <mail@fiegel.io>
2011-10-10 13:40:05 +0200

    removed hardcoded block references from sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,4 @@
     <div id="sidebar">
     <!-- Call your plugins theme functions here-->
         <?php $theme->area('sidebar'); ?>
-		<?php $theme->twitter(); ?>
-        <?php $theme->show_recentcomments(); ?>
     </div>
