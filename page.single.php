ab3e15222f5f2fa892498665d5bced1c074bbea9
Florian Fiegel <mail@fiegel.io>
2011-10-11 15:29:32 +0200

    added edit link on pages. closes #1

--- page.single.php
+++ page.single.php
@@ -2,6 +2,7 @@
         <div id="page">
             <div id="main">
                 <div class="post">
+					<?php if ( $loggedin ) : ?><small class="date"><a href="<?php echo $post->editlink; ?>">EDIT</a></small><?php endif; ?>
                     <h2><a href="<?php echo $post->permalink; ?>"><?php echo $post->title; ?></a></h2>
                     <?php echo $post->content_out; ?>
                 </div>
