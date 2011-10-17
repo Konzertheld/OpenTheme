fa67983998abd61124b81b3387902b08ffecd00e
Florian Fiegel <mail@fiegel.io>
2011-10-17 15:17:01 +0200

    Fixing Bug on Commentsform and beautify gravatar integration

--- comments.php
+++ comments.php
@@ -4,7 +4,8 @@
 			<ol class="commentlist">
 		        <?php foreach ( $post->comments->moderated as $comment ) : ?>
 				<li id="comment-<?php echo $comment->id; ?>">
-			    <small><a href="<?php echo $comment->url; ?>" rel="external" class="url"><?php echo $comment->name; ?></a> am <?php $comment->date->out(); ?></small>
+					<?php if ( Plugins::is_loaded('Gravatar') ) : ?><img src="<?php echo $comment->gravatar; ?>" class="gravatar" /><?php endif; ?>
+			    <p class="comment-meta"><a href="<?php echo $comment->url; ?>" rel="external" class="url"><?php echo $comment->name; ?></a><br /><?php $comment->date->out(); ?></p>
 			<?php echo $comment->content_out; ?>
 			<?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?>
 			<p><?php _e( 'Your comment is awaiting moderation' ) ;?></p>
